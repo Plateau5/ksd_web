104c0b770830483882d033216e4809a86887dc74
arley <arleyjoe@163.com>
2018-01-09 15:16:58 +0800

    修改车辆信息数据渲染 2018-1-9 15:16:51

--- controller/viewsController/dockingController.js
+++ controller/viewsController/dockingController.js
@@ -41,6 +41,7 @@ exports.VIEW_DOCKING_PINGAN_CAR = function(req, res, next) {
         title : '客户-车辆信息',
         page : './customer/dockingPACarInfo',
         callback : function (data) {
+            formatCarInfoAmount(data);
             var queryType = req.body.query_type;
             var finance_id = req.body.finance_id;
             data.query_type = queryType;
@@ -118,6 +119,12 @@ exports.VIEW_DOCKING_PINGAN_CREDIT = function(req, res, next) {
 };
 
 
+/**
+ * 文件信息部分数据格式化
+ * @author Arley Joe 2018-1-4 14:39:06
+ * @param d {Object} : 源数据
+ * @return {Array}  ：返回值
+ */
 var organizeData = function (d) {
     var dataFiles = [];
     var materialSeries = [];
@@ -142,7 +149,21 @@ var organizeData = function (d) {
 };
 
 
-
+/**
+ * 格式化金额数据
+ * @author Arley Joe 2018-1-9 14:47:47
+ * @param d
+ */
+function formatCarInfoAmount (d) {
+    (!d.buyTax && d.buyTax != 0) && (d.buyTax = 0);     // 购置税
+    (!d.pawnValue && d.pawnValue != 0) && (d.pawnValue = 0);    // 车辆实际价格
+    (!d.otherFee && d.otherFee != 0) && (d.otherFee = 0);       // 其他费用
+    (!d.assure && d.assure != 0) && (d.assure = 0);     // 保险
+    (!d.firstPayScale && d.firstPayScale != 0) && (d.firstPayScale = '20%');    // 首付比例
+    (!d.cautionMoney && d.cautionMoney != 0) && (d.cautionMoney = 0);       // 保证金
+    (!d.serviceCharge && d.serviceCharge != 0) && (d.serviceCharge = 0);        // 服务费
+    (!d.attachFinance && d.attachFinance != 0) && (d.attachFinance = 0);        // 附加融资额
+}
 
 
 
