d39df1392564f476272f16b1320f0e2f65255bff
arley <arleyjoe@163.com>
2018-01-24 17:46:52 +0800

    打开日志  2018年1月24日17:46:48

--- util/logger.js
+++ util/logger.js
@@ -6,16 +6,16 @@ var logger = require('./log4Config');
 
 exports.logError = function (msg) {
     var logError = logger('error');
-    if (apiServerPath.indexOf('kuaishoudan.com') !== -1) {
+    // if (apiServerPath.indexOf('kuaishoudan.com') !== -1) {
         logError.error(msg);
-    }
+    // }
     console.error(msg);
 };
 
 exports.logInfo = function (msg) {
     var logInfo = logger('info');
-    if (apiServerPath.indexOf('kuaishoudan.com') !== -1) {
+    // if (apiServerPath.indexOf('kuaishoudan.com') !== -1) {
         logInfo.info(msg);
-    }
+    // }
     console.log(msg);
 };
